b0095bdcdda3e8d32cb883820516f293c936f959
greisonsantos <greisonsantos03@gmail.com>
2020-02-05 22:15:09 -0300

    init insert idShipment

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -12,7 +12,8 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./styles.css";
 import api from '../../services/api'
 import swal from 'sweetalert';
-import { format,addHours } from 'date-fns'
+import { Modal, Button } from 'react-bootstrap'
+import { format, addHours } from 'date-fns'
 const Home = (props) => {
 
   const UpdateTruck = async (truckId, status) => {
@@ -39,7 +40,25 @@ const Home = (props) => {
     localStorage.removeItem('@TOKEN');
     props.history.push('/')
   }
- 
+
+
+  const [show, setShow] = useState(false);
+  const [IdShipment, setIdShipment] = useState('')
+  const handleClose = () => setShow(false);
+
+  const ModalIdShipment = async (truck_id, value) => {
+
+    setShow(true)
+    setIdShipment(value)
+    // const response=api.put('/')
+  }
+
+  const saveIdShipment = async () => {
+
+    // const response
+
+  }
+
   useEffect(() => {
 
     async function loadTrucks() {
@@ -50,7 +69,7 @@ const Home = (props) => {
           return {
             ...truck,
             data_posicao: format(
-              addHours(new Date(truck.data_posicao),3),
+              addHours(new Date(truck.data_posicao), 3),
               "dd/MM/yyyy HH:mm"),
             status: truck.status === true ? <div style={{ fontSize: 18, color: '#3CB310' }}>Enviando dados</div> : <div style={{ color: '#8B0000' }}>Não está enviando dados</div>,
             ignicao: truck.ignicao === true ? <div style={{ fontSize: 15, color: '#3CB310' }}>Ligada</div> : <div style={{ color: '#8B0000' }}>Desligada</div>,
@@ -63,11 +82,21 @@ const Home = (props) => {
                 Alterar Status
           </button>
             ),
+            IdShipment: (
+              <button onDoubleClick={() => ModalIdShipment(truck.id, truck.id)}
+                style={{ width: 90, backgroundColor: "#000080", color: '#FFF', padding: 7, borderRadius: 8 }}
+                className="details">
+
+                64654646
+              </button>
+
+            )
+
           }
         });
 
-        setDataTable(dataTable=>({ ...dataTable, rows: trucks }))
-      } 
+        setDataTable(dataTable => ({ ...dataTable, rows: trucks }))
+      }
     }
     loadTrucks();
   }, [])
@@ -118,6 +147,12 @@ const Home = (props) => {
         width: 100
       },
       {
+        label: "Id de Envio",
+        field: "IdShipment",
+        sort: 'asc',
+        width: 100
+      },
+      {
         label: "Acão",
         field: "action",
         sort: 'asc',
@@ -148,11 +183,11 @@ const Home = (props) => {
           <MDBCardBody>
             <h6 className="card-title">Lista de caminhões</h6>
             <MDBDataTable
-            displayEntries={true}
-            entriesLabel={'Escolha o numero de linhas'}
-            searchLabel={'Pesquisar'}
-            paginationLabel={['Anterior','Proximo']}
-            infoLabel={['Mostrando','à','de','registros']}
+              displayEntries={true}
+              entriesLabel={'Escolha o numero de linhas'}
+              searchLabel={'Pesquisar'}
+              paginationLabel={['Anterior', 'Proximo']}
+              infoLabel={['Mostrando', 'à', 'de', 'registros']}
               striped
               bordered
               small
@@ -161,6 +196,29 @@ const Home = (props) => {
           </MDBCardBody>
         </MDBCard>
       </div>
+
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Id de Envio</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <input
+            style={{ display: 'flex', flex: 1, flexDirection: 'row', alignContent: 'center', alignItems: 'center', height: 44, borderRadius: 10, borderWidth: 1 }}
+            value={IdShipment}
+            onChange={(e) => setIdShipment(e.target.value)}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Salvar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
     </div>
   )
 };
