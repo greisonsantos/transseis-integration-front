df8566d2d7637fd4fd807e8fc23e0fadaaac4449
unknown <breno_lopes_10@yahoo.com.br>
2020-02-14 14:38:25 -0300

    adição do dotenv

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+require('dotenv/config');
 
 const app = express()
 
@@ -7,5 +8,4 @@ const baseDir = `${__dirname}/build/`
 app.get('*', (req,res) => res.sendFile('index.html' , { root : baseDir }))
 
 const port = 4000
-
 app.listen(port, () => console.log(`Servidor subiu com sucesso em http://localhost:${port}`))
